ff2cf61a441bc48c4cdc9eb61b0fe0afc593625e
Vimmi <vimmigujral@gmail.com>
2023-08-14 01:16:09 -0500

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://mongo:27017';
+const url         = 'mongodb://localhost:27017';
 let db            = null;
  
 // connect to mongo
